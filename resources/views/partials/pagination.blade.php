a44fbd33a4ef69d601317717186bc3431761ae0d
Robin Valdmets <robin.valdmets@gmail.com>
2023-12-18 20:54:30 +0200

    aa

--- resources/views/partials/pagination.blade.php
+++ resources/views/partials/pagination.blade.php
@@ -1,39 +1,23 @@
 @if ($paginator->hasPages())
-    <div class="join">
+    <div class="join grid grid-cols-2" style="display:flex; justify-content:center; margin-bottom: 2%;">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <a class="join-item btn btn-disabled" aria-hidden="true" aria-disabled="true"
-               aria-label="@lang('pagination.previous')">&lsaquo;</a>
+            <a class="join-item btn btn-outline disabled" style="width: 200px;" aria-disabled="true">@lang('pagination.previous')</a>
         @else
-            <a class="join-item btn" href="{{ $paginator->previousPageUrl() }}" rel="prev"
-               aria-label="@lang('pagination.previous')">&lsaquo;</a>
+            <a href="{{ $paginator->previousPageUrl() }}" style="width: 200px;" class="join-item btn btn-outline" rel="prev">
+                <span>@lang('pagination.previous')</span>
+            </a>
         @endif
-
-        {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <a class="join-item btn btn-disabled" aria-disabled="true">{{ $element }}</a>
-            @endif
-
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <a class="join-item btn btn-active" aria-current="page">{{ $page }}</a>
-                    @else
-                        <a href="{{ $url }}" class="join-item btn">{{ $page }}</a>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" class="join-item btn" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
+            <a href="{{ $paginator->nextPageUrl() }}" style="width: 200px;" class="join-item btn btn-outline" rel="next">
+                <span>@lang('pagination.next')</span>
+            </a>
         @else
-            <a class="join-item btn btn-disabled" aria-disabled="true" aria-label="@lang('pagination.next')"
-               aria-hidden="true">&rsaquo;</a>
+            <a class="join-item btn btn-outline disabled" style="width: 200px;" aria-disabled="true">
+                <span>@lang('pagination.next')</span>
+            </a>
         @endif
     </div>
 @endif
